5e26ea242302b8dd46321643429a47946469342c
kevin <knitschmann@mailbox.tu-berlin.de>
2015-08-25 12:47:08 +0200

    successfully made JDBC query to retrieve book data from XAMPP MYSQL server

--- MyJDBC/src/main/java/DBMetaData.java
+++ MyJDBC/src/main/java/DBMetaData.java
@@ -1,39 +1,55 @@
 /**
  * Created by kevin on 25/08/15.
  */
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.Properties;
+
 public class DBMetaData {
-    public static void main(String[] args)throws Exception{
+    public static void main(String[] args) throws Exception {
         FileInputStream fin = null;
         Properties prop = new Properties();
-        try {
-            fin = new FileInputStream("MyJDBC/dbconnect.properties");
-            prop.load(fin);
-            fin.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        fin = new FileInputStream("MyJDBC/dbconnect.properties");
+        prop.load(fin);
+        fin.close();
 
+        //read all settings from dbconnect.properties
         String driver = prop.getProperty("driver");
         String url = prop.getProperty("url");
         String user = prop.getProperty("user");
         String password = prop.getProperty("password");
 
-        if (driver != null){
-                Class.forName(driver);
+        if (driver != null) {
+            Class.forName(driver);
+        } else {
+            //Standard Driver for XAMP MYSQL
+            Class.forName("com.mysql.jdbc.Driver");
         }
         Connection con = DriverManager.getConnection(url, user, password);
+
+        //Metadata to test connection
         DatabaseMetaData dbmd = con.getMetaData();
-        System.out.println("URL "+ dbmd.getURL());
-        System.out.println("User "+ dbmd.getUserName());
-        System.out.println("DatabaseProductname "+dbmd.getDatabaseProductName());
-        System.out.println("");
+        System.out.println("URL " + dbmd.getURL());
+        System.out.println("User " + dbmd.getUserName());
+        System.out.println("DatabaseProductname " + dbmd.getDatabaseProductName());
+
+        //make a Query
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM buch");
+
+        //output Result Set of Query
+        while (rs.next()) {
+            System.out.println(
+                    rs.getString("autor") + ", " +
+                            rs.getString("titel")
+
+            );
+        }
+        rs.close();
+        st.close();
         con.close();
     }
 }
