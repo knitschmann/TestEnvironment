a82a469002a35145a13aa949e91cd4f64a4d7278
kevin <knitschmann@mailbox.tu-berlin.de>
2015-09-18 14:12:33 +0200

    init commit SpringDemo

---
+++ SpringDemo/src/main/java/SpringDemo/Greeting.java
@@ -0,0 +1,23 @@
+package SpringDemo;
+
+/**
+ * Created by kevin on 18/09/15.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ SpringDemo/src/main/java/SpringDemo/GreetingController.java
@@ -0,0 +1,29 @@
+package SpringDemo;
+
+/**
+ * Created by kevin on 18/09/15.
+ */
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.transform.Result;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        return "redirect:index.html";
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @RequestMapping(value ="/greetings", method = RequestMethod.POST)
+    @ResponseBody
+    Greeting home(@RequestBody String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    }
+}
