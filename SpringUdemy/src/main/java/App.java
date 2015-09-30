b597f8716e97184a461323318be16a558539dd8d
kevin <knitschmann@mailbox.tu-berlin.de>
2015-09-30 13:37:15 +0200

    created spring bean in IntelliJ Enterprise

--- SpringUdemy/src/main/java/App.java
+++ SpringUdemy/src/main/java/App.java
@@ -1,8 +1,13 @@
+import org.springframework.context.ApplicationContext;
+
 /**
  * Created by kevin on 30/09/15.
  */
 public class App {
     public static void main(String[] args) {
+
+        ApplicationContext context;
+
         Person person = new Person();
         person.speak();
     }
